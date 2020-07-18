2ba29269ad63add04d67bb9578de35b968c39568
A03ki <a03ki04@gmail.com>
2020-07-18 19:56:11 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md')) as f:
+    readme = f.read()
+
+setup(
+    name='torchvision4ad',
+    version='0.1.0',
+    description='torchvision for anomaly detection',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='A03ki',
+    author_email='a03ki04@gmail.com',
+    install_requires=['torchvision'],
+    url='https://github.com/A03ki/torchvision4ad',
+    license='MIT',
+    packages=['torchvision4ad'],
+    keywords='torchvision anomaly detection',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+    ]
+)
